2fd338cdf51b0d84662c9d58598bfc5fca43c3e5
hieucv25 <chuhieu05@gmail.com>
2023-07-13 10:18:44 +0700

    upload

--- my-app/src/Components/Admin/IndexDemo.js
+++ my-app/src/Components/Admin/IndexDemo.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import "../../../css/admin.css";
 import { Link } from "react-router-dom";
-import { Sidebar } from "/Layout/Sidebar";
+import Sidebar from "./Layout/Sidebar";
 
 export default function IndexDV() {
   return (
@@ -34,44 +33,42 @@ export default function IndexDV() {
                   </tr>
                 </thead>
                 <tbody>
-                  {/* {ListDV.map((dv, index) => (
-                    <tr>
-                      <th scope="row" key={index}>
-                        <td>{index + 1}</td>
-                      </th>
-                      <td>{dv.ten}</td>
-                      <td>{dv.donGia}</td>
-                      <td>Phụ Kiện</td>
-                      <td>{dv.loaiDV.ten}</td>
-                      <td>
-                        <Link
-                          className="btn btn-sm btn-secondary "
-                        >
-                          Edit
-                        </Link>
-                        <span className="padd"></span>
-                        <button
-                          className="btn btn-sm btn-danger "
-                        >
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  ))} */}
+                  <tr>
+                    <th>
+                      <td>ABC</td>
+                    </th>
+                    <td>ABC</td>
+                    <td>ABC</td>
+                    <td>ABC</td>
+                    <td>ABC</td>
+                    <td>
+                      <Link
+                        className="btn btn-sm btn-secondary "
+                      >
+                        Edit
+                      </Link>
+                      <span className="padd"></span>
+                      <button
+                        className="btn btn-sm btn-danger "
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
                 </tbody>
               </table>
               <div className="px-5 py-2 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between">
                 <div className="inline-flex mt-2 mt-0">
                   <button
                     className="btn btn-sm btn-outline-secondary"
-                    // onClick={prevPage}
+                  // onClick={prevPage}
                   >
                     Prev
                   </button>
 
                   <button
                     className="btn btn-sm btn-outline-secondary"
-                    // onClick={nextPage}
+                  // onClick={nextPage}
                   >
                     Next
                   </button>
